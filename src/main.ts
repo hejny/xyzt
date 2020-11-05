5c21f1d57e9b5fa86d1346c8bc5301d5c5804ab7
Pavol Hejny <me@pavolhejny.com>
2020-11-05 03:50:44 +0100

    Updating exports

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+// GENERATED WITH generate-main-exports
+// Warning: Do not edit by hand, all changes will be lost on next execution!
+
+import { Transform } from './classes/Transform';
+import { Vector } from './classes/Vector';
+import { ITransform } from './interfaces/ITransform';
+import { IVector } from './interfaces/IVector';
+import { transformFromCss } from './utils/transformFromCss';
+import { transformToCss } from './utils/transformToCss';
+
+export {
+    Vector,
+    IVector,
+    Transform,
+    ITransform,
+    transformToCss,
+    transformFromCss,
+};
