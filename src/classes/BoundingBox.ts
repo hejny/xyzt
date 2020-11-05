2ff0ae1342cc2cebb6e11847952d5a7a34ce05db
Pavol Hejny <me@pavolhejny.com>
2020-11-05 05:44:21 +0100

    BoundingBox

---
+++ src/classes/BoundingBox.ts
@@ -0,0 +1,29 @@
+import { IBoundingBox } from '../interfaces/IBoundingBox';
+import { ITransform } from '../interfaces/ITransform';
+import { Transform } from './Transform';
+import { Vector } from './Vector';
+
+
+export class BoundingBox implements IBoundingBox{
+
+    public static fromTransform( transform: ITransform):BoundingBox{
+        return new BoundingBox(Transform.fromObject(transform));
+    }
+
+    private constructor(public transform: Transform){
+    }
+
+    
+    public get center(): Vector{
+        // TODO: Rotate
+        return this.transform.translate.add(this.transform.scale.half())
+    }
+
+    // TODO: Other corners
+
+
+    public applyTransform(transform: ITransform){
+        this.transform = Transform.combine(this.transform,transform);
+    }
+
+}
---
+++ src/interfaces/IBoundingBox.ts
@@ -0,0 +1,6 @@
+import { ITransform } from "./ITransform";
+
+export interface IBoundingBox {
+    transform: ITransform;
+    applyTransform(transform: ITransform): void;
+}
