edc643ad7a6c82a5574746a6b583d67bbe75b581
Pavol Hejny <me@pavolhejny.com>
2020-11-12 01:35:30 +0100

    Prettier

--- test/BoundingBox.test.ts
+++ test/BoundingBox.test.ts
@@ -4,8 +4,12 @@ import { Vector } from '../src/classes/Vector';
 
 describe('BoundingBox', () => {
     it('Constructing', () => {
-        expect(()=>BoundingBox.one()).not.toThrowError();
-        expect(()=>BoundingBox.fromTransform(Transform.fromObject({translate: new Vector(1,2)}))).not.toThrowError();
+        expect(() => BoundingBox.one()).not.toThrowError();
+        expect(() =>
+            BoundingBox.fromTransform(
+                Transform.fromObject({ translate: new Vector(1, 2) }),
+            ),
+        ).not.toThrowError();
     });
 
     // TODO: Other methods and scenarios
