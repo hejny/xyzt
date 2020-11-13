218774ef80dc416862b01374e5a40045399b618e
Pavol Hejny <me@pavolhejny.com>
2020-11-13 22:25:56 +0100

    Extracting values from CSS/SVG style

---
+++ src/utils/extractValuesFromStyle.ts
@@ -0,0 +1,31 @@
+export function extractValuesFromStyle(
+    functionName: string,
+    cssTransform: string,
+): Array<{ unit?: string; value: number }> | null {
+    const matchInner = new RegExp(
+        `${functionName}\\((?<inner>.*)\\)`,
+        'is',
+    ).exec(cssTransform);
+
+    if (!matchInner || !matchInner.groups || !matchInner.groups.inner) {
+        return null;
+    }
+
+    const parts = matchInner.groups.inner
+        .split(/\s*[\,\s]\s*/gs)
+        .filter((part) => part !== '');
+
+    const values = parts.map((part) => {
+        const matchPart = /^(?<value>\-?\d+(\.\d+)?)(?<unit>.*)$/.exec(part);
+
+        if (!matchPart || !matchPart.groups) {
+            return { value: 0 };
+        }
+
+        const { value, unit } = matchPart.groups;
+
+        return { value: parseFloat(value), unit };
+    });
+
+    return values;
+}
---
+++ test/extractValuesFromStyle.test.ts
@@ -0,0 +1,89 @@
+import { extractValuesFromStyle } from '../src/utils/extractValuesFromStyle';
+
+describe('Extracting values from CSS/SVG style', () => {
+    it('extracts correct values', () => {
+        expect(
+            extractValuesFromStyle(`translate`, `translate(1px,2px,3px)`)?.map(
+                ({ value }) => value,
+            ),
+        ).toEqual([1, 2, 3]);
+        expect(
+            extractValuesFromStyle(`translate`, `translate(1px 2px 3px)`)?.map(
+                ({ value }) => value,
+            ),
+        ).toEqual([1, 2, 3]);
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(1.5px,2.1px,0.5px)`,
+            )?.map(({ value }) => value),
+        ).toEqual([1.5, 2.1, 0.5]);
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(-1.5px -2.1px -0.5px)`,
+            )?.map(({ value }) => value),
+        ).toEqual([-1.5, -2.1, -0.5]);
+    });
+
+    it('can deal with extra spaces and formating', () => {
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(           -1.5px         -2.1px
+                
+                -0.5px)`,
+            )?.map(({ value }) => value),
+        ).toEqual([-1.5, -2.1, -0.5]);
+    });
+
+    it('can deal with incorrect input', () => {
+        expect(
+            extractValuesFromStyle(`translate`, `foo(-1.5px -2.1px -0.5px)`),
+        ).toEqual(null);
+
+        // TODO: More
+    });
+
+    it('extracts correct units', () => {
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(-1.5px -2.1px -0.5px)`,
+            )?.map(({ unit }) => unit),
+        ).toEqual(['px', 'px', 'px']);
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(-1.5% -2.1% -0.5vh)`,
+            )?.map(({ unit }) => unit),
+        ).toEqual(['%', '%', 'vh']);
+        expect(
+            extractValuesFromStyle(`rotate`, `rotate(25grad)`)?.map(
+                ({ unit }) => unit,
+            ),
+        ).toEqual(['grad']);
+    });
+
+    it('extracts correct values and units', () => {
+        expect(
+            extractValuesFromStyle(
+                `translate`,
+                `translate(-1.5px -2.1px -0.5px)`,
+            ),
+        ).toEqual([
+            {
+                value: -1.5,
+                unit: 'px',
+            },
+            {
+                value: -2.1,
+                unit: 'px',
+            },
+            {
+                value: -0.5,
+                unit: 'px',
+            },
+        ]);
+    });
+});
