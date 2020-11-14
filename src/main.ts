68a404240b8cab1cbdb8bbcf9a1a5b6a11cd0421
Pavol Hejny <me@pavolhejny.com>
2020-11-14 02:56:14 +0100

    Updating exports

--- src/main.ts
+++ src/main.ts
@@ -16,26 +16,22 @@ import { showValueWithUnit } from './utils/showValueWithUnit';
 import { applyTransformOnElement } from './utils/transform/applyTransformOnElement/applyTransformOnElement';
 import { applyTransformOnHtmlElement } from './utils/transform/applyTransformOnElement/applyTransformOnHtmlElement';
 import { applyTransformOnSvgElement } from './utils/transform/applyTransformOnElement/applyTransformOnSvgElement';
-import {
-    IAngleUnit,
-    IDimensionUnit,
-    ITransformStyleFormat,
-} from './utils/transform/styleFormat/ITransformStyleFormat';
-import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
+import { IDimensionUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
+import { IAngleUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
+import { ITransformStyleFormat } from './utils/transform/styleFormat/ITransformStyleFormat';
 import { _rotateFromStyle } from './utils/transform/transformFromStyle/_rotateFromStyle';
 import { _scaleFromStyle } from './utils/transform/transformFromStyle/_scaleFromStyle';
 import { _translateFromStyle } from './utils/transform/transformFromStyle/_translateFromStyle';
-import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
-import {
-    CSS_FORMAT,
-    transformToStyleCss,
-} from './utils/transform/transformToStyle/transformToStyleCss';
-import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
+import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
 import { _transformAngleToStyle } from './utils/transform/transformToStyle/_transformAngleToStyle';
 import { _transformDimensionToStyle } from './utils/transform/transformToStyle/_transformDimensionToStyle';
 import { _transformRotateToStyle } from './utils/transform/transformToStyle/_transformRotateToStyle';
 import { _transformScaleToStyle } from './utils/transform/transformToStyle/_transformScaleToStyle';
 import { _transformTranslateToStyle } from './utils/transform/transformToStyle/_transformTranslateToStyle';
+import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
+import { CSS_FORMAT } from './utils/transform/transformToStyle/transformToStyleCss';
+import { transformToStyleCss } from './utils/transform/transformToStyle/transformToStyleCss';
+import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
 
 export {
     modulo,
