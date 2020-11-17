54a3621ca1ad6569e54716c47704349395e30537
Pavol Hejny <me@pavolhejny.com>
2020-11-17 20:29:18 +0100

    Prettier

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { Transform } from './classes/Transform';
 import { Vector } from './classes/Vector';
 import { IBoundingBox } from './interfaces/IBoundingBox';
 import { ITransform } from './interfaces/ITransform';
-import { IVector } from './interfaces/IVector';
-import { IVectorObject } from './interfaces/IVector';
-import { IAxis } from './interfaces/IVector';
-import { AXIS } from './interfaces/IVector';
+import { AXIS, IAxis, IVector, IVectorObject } from './interfaces/IVector';
 import { convertAngle } from './utils/convertAngle';
 import { extractValuesFromStyle } from './utils/extractValuesFromStyle';
 import { fullAngle } from './utils/fullAngle';
@@ -19,22 +16,26 @@ import { showValueWithUnit } from './utils/showValueWithUnit';
 import { applyTransformOnElement } from './utils/transform/applyTransformOnElement/applyTransformOnElement';
 import { applyTransformOnHtmlElement } from './utils/transform/applyTransformOnElement/applyTransformOnHtmlElement';
 import { applyTransformOnSvgElement } from './utils/transform/applyTransformOnElement/applyTransformOnSvgElement';
-import { IDimensionUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
-import { IAngleUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
-import { ITransformStyleFormat } from './utils/transform/styleFormat/ITransformStyleFormat';
+import {
+    IAngleUnit,
+    IDimensionUnit,
+    ITransformStyleFormat,
+} from './utils/transform/styleFormat/ITransformStyleFormat';
+import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
 import { _rotateFromStyle } from './utils/transform/transformFromStyle/_rotateFromStyle';
 import { _scaleFromStyle } from './utils/transform/transformFromStyle/_scaleFromStyle';
 import { _translateFromStyle } from './utils/transform/transformFromStyle/_translateFromStyle';
-import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
+import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
+import {
+    CSS_FORMAT,
+    transformToStyleCss,
+} from './utils/transform/transformToStyle/transformToStyleCss';
+import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
 import { _transformAngleToStyle } from './utils/transform/transformToStyle/_transformAngleToStyle';
 import { _transformDimensionToStyle } from './utils/transform/transformToStyle/_transformDimensionToStyle';
 import { _transformRotateToStyle } from './utils/transform/transformToStyle/_transformRotateToStyle';
 import { _transformScaleToStyle } from './utils/transform/transformToStyle/_transformScaleToStyle';
 import { _transformTranslateToStyle } from './utils/transform/transformToStyle/_transformTranslateToStyle';
-import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
-import { CSS_FORMAT } from './utils/transform/transformToStyle/transformToStyleCss';
-import { transformToStyleCss } from './utils/transform/transformToStyle/transformToStyleCss';
-import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
 
 export {
     AXIS,
