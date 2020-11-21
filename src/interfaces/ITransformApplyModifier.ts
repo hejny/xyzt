aa066e537857c2038f2b9dbb01fb197301b96f69
Pavol Hejny <me@pavolhejny.com>
2020-11-21 02:13:01 +0100

    Prettier

--- src/interfaces/ITransformApplyModifier.ts
+++ src/interfaces/ITransformApplyModifier.ts
@@ -1,7 +1,9 @@
 import { Transform } from '../classes/Transform';
 import { ITransform } from './ITransform';
 
-export type ITransformApplyModifierFunction = (Transform: Transform) => ITransform;
+export type ITransformApplyModifierFunction = (
+    Transform: Transform,
+) => ITransform;
 export type IAppliableOnTransform = {
     applyOnTransform: ITransformApplyModifierFunction;
 };
