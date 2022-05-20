ab352a680473ca1dcf95482d0013417f0d7019a5
Pavol Hejny <me@pavolhejny.com>
2022-05-20 18:00:17 +0200

    Main exports

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,19 @@ import { Vector } from './classes/Vector';
 import { NEGLIGIBLE_THRESHOLD } from './config';
 import { IBoundingBox } from './interfaces/IBoundingBox';
 import { IInversible } from './interfaces/IInversible';
-import {
-    IAppliableOnTransform,
-    ITransformApplyModifier,
-    ITransformApplyModifierFunction,
-} from './interfaces/ITransformApplyModifier';
+import { ITransformApplyModifierFunction } from './interfaces/ITransformApplyModifier';
+import { IAppliableOnTransform } from './interfaces/ITransformApplyModifier';
+import { ITransformApplyModifier } from './interfaces/ITransformApplyModifier';
 import { ITransformData } from './interfaces/ITransformData';
-import {
-    IAppliableOnVector,
-    IVectorApplyModifier,
-    IVectorApplyModifierFunction,
-} from './interfaces/IVectorApplyModifier';
-import { AXES, IAxis, IVector, IVectorData } from './interfaces/IVectorData';
+import { IVectorApplyModifierFunction } from './interfaces/IVectorApplyModifier';
+import { IAppliableOnVector } from './interfaces/IVectorApplyModifier';
+import { IVectorApplyModifier } from './interfaces/IVectorApplyModifier';
+import { IVector } from './interfaces/IVectorData';
+import { IVectorData } from './interfaces/IVectorData';
+import { IAxis } from './interfaces/IVectorData';
+import { AXES } from './interfaces/IVectorData';
+import { ArrayFull } from './interfaces/typeHelpers';
+import { ArrayFull2 } from './interfaces/typeHelpers';
 import { convertAngle } from './utils/convertAngle';
 import { extractValuesFromStyle } from './utils/extractValuesFromStyle';
 import { fullAngle } from './utils/fullAngle';
@@ -29,62 +30,67 @@ import { stripInfatesimal } from './utils/stripInfatesimal';
 import { applyTransformOnElement } from './utils/transform/applyTransformOnElement/applyTransformOnElement';
 import { applyTransformOnHtmlElement } from './utils/transform/applyTransformOnElement/applyTransformOnHtmlElement';
 import { applyTransformOnSvgElement } from './utils/transform/applyTransformOnElement/applyTransformOnSvgElement';
-import { IAngleUnit, IDimensionUnit, ITransformStyleFormat } from './utils/transform/styleFormat/ITransformStyleFormat';
-import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
+import { IDimensionUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
+import { IAngleUnit } from './utils/transform/styleFormat/ITransformStyleFormat';
+import { ITransformStyleFormat } from './utils/transform/styleFormat/ITransformStyleFormat';
 import { _rotateFromStyle } from './utils/transform/transformFromStyle/_rotateFromStyle';
 import { _scaleFromStyle } from './utils/transform/transformFromStyle/_scaleFromStyle';
 import { _translateFromStyle } from './utils/transform/transformFromStyle/_translateFromStyle';
-import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
-import { CSS_FORMAT, transformToStyleCss } from './utils/transform/transformToStyle/transformToStyleCss';
-import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
+import { transformFromStyle } from './utils/transform/transformFromStyle/transformFromStyle';
 import { _transformAngleToStyle } from './utils/transform/transformToStyle/_transformAngleToStyle';
 import { _transformDimensionToStyle } from './utils/transform/transformToStyle/_transformDimensionToStyle';
 import { _transformRotateToStyle } from './utils/transform/transformToStyle/_transformRotateToStyle';
 import { _transformScaleToStyle } from './utils/transform/transformToStyle/_transformScaleToStyle';
 import { _transformTranslateToStyle } from './utils/transform/transformToStyle/_transformTranslateToStyle';
+import { transformToStyle } from './utils/transform/transformToStyle/transformToStyle';
+import { CSS_FORMAT } from './utils/transform/transformToStyle/transformToStyleCss';
+import { transformToStyleCss } from './utils/transform/transformToStyle/transformToStyleCss';
+import { transformToStyleSvg } from './utils/transform/transformToStyle/transformToStyleSvg';
 
 export {
-    AXES,
-    IAxis,
-    modulo,
-    Vector,
-    IVector,
-    showValue,
-    fullAngle,
-    Transform,
-    CSS_FORMAT,
-    IAngleUnit,
-    IVectorData,
-    IInversible,
-    BoundingBox,
-    convertAngle,
-    IBoundingBox,
-    IDimensionUnit,
-    ITransformData,
-    _scaleFromStyle,
-    transformToStyle,
-    _rotateFromStyle,
-    stripInfatesimal,
-    showValueWithUnit,
-    transformFromStyle,
-    IAppliableOnVector,
-    transformToStyleSvg,
-    transformToStyleCss,
-    _translateFromStyle,
-    IVectorApplyModifier,
-    NEGLIGIBLE_THRESHOLD,
-    ITransformStyleFormat,
-    IAppliableOnTransform,
-    _transformScaleToStyle,
-    _transformAngleToStyle,
-    extractValuesFromStyle,
-    _transformRotateToStyle,
-    applyTransformOnElement,
-    ITransformApplyModifier,
-    _transformTranslateToStyle,
-    _transformDimensionToStyle,
-    applyTransformOnSvgElement,
-    applyTransformOnHtmlElement,
-    IVectorApplyModifierFunction,
-    ITransformApplyModifierFunction,
+AXES,
+IAxis,
+modulo,
+Vector,
+IVector,
+showValue,
+fullAngle,
+ArrayFull,
+Transform,
+CSS_FORMAT,
+IAngleUnit,
+ArrayFull2,
+IVectorData,
+IInversible,
+BoundingBox,
+convertAngle,
+IBoundingBox,
+IDimensionUnit,
+ITransformData,
+_scaleFromStyle,
+transformToStyle,
+_rotateFromStyle,
+stripInfatesimal,
+showValueWithUnit,
+transformFromStyle,
+IAppliableOnVector,
+transformToStyleSvg,
+transformToStyleCss,
+_translateFromStyle,
+IVectorApplyModifier,
+NEGLIGIBLE_THRESHOLD,
+ITransformStyleFormat,
+IAppliableOnTransform,
+_transformScaleToStyle,
+_transformAngleToStyle,
+extractValuesFromStyle,
+_transformRotateToStyle,
+applyTransformOnElement,
+ITransformApplyModifier,
+_transformTranslateToStyle,
+_transformDimensionToStyle,
+applyTransformOnSvgElement,
+applyTransformOnHtmlElement,
+IVectorApplyModifierFunction,
+ITransformApplyModifierFunction
 };
