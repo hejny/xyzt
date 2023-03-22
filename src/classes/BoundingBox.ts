9017b91d9889314b21f24552fecd419ca987156e
Pavol Hejny <me@pavolhejny.com>
2023-03-23 00:37:46 +0100

    Revert "🧽 Normalize TODOs"
    This reverts commit f3efdb7ae5876564a38dbd88601dba44cd7a16b6.
    
    🔼 This commit was automatically generated by [Batch project editor](https://github.com/hejny/batch-project-editor) using workflow onceRevertLastBpeCommit

--- src/classes/BoundingBox.ts
+++ src/classes/BoundingBox.ts
@@ -162,7 +162,7 @@ export class BoundingBox implements IBoundingBox {
         );
     }
 
-    // TODO: !! TODO: intersects for another BoundingBox
+    // !! TODO: intersects for another BoundingBox
 
     public applyTransform(transform: ITransformData) {
         // TODO: Same pattern as Vector and Transform
